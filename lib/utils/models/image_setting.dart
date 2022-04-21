f1287d66fd776b99bce4a84cf9338d1f04253423
GiudaBallerino <lorispesarin@gmail.com>
2022-04-21 20:47:15 +0200

    added ImageSetting model

---
+++ lib/utils/models/image_setting.dart
@@ -0,0 +1,19 @@
+class ImageSetting{
+  final String path;
+  double x;
+  double y;
+  double width;
+  double height;
+  double opacity;
+  double rotation;
+
+  ImageSetting({
+    required this.path,
+    required this.x,
+    required this.y,
+    required this.width,
+    required this.height,
+    required this.opacity,
+    required this.rotation,
+  });
+}
