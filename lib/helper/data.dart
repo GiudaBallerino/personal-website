f327162caf1380106df73e6aa229ea716067ed97
GiudaBallerino <lorispesarin@gmail.com>
2022-04-26 18:08:28 +0200

    added new data

--- lib/helper/data.dart
+++ lib/helper/data.dart
@@ -1,15 +1,17 @@
+import 'package:personal_website/models/widget_furniture.dart';
+
 import '../utils/constant.dart';
-import '../utils/models/page.dart';
+import '../models/page.dart';
 import 'package:flutter/material.dart';
 
-class PagesData{
+class PagesData {
   List<Pages> list = [
     Pages(
         id: "art",
         name: ".art()",
         route: "/launch",
         type: PagesType.impress,
-        logo:const Icon(
+        logo: const Icon(
           Icons.brush,
           color: kTextColor,
         )),
@@ -21,12 +23,30 @@ class PagesData{
         logo: const Icon(Icons.home, color: kTextColor)),
   ];
 }
-class ImageCategory{
-  List<String> list=[
-    'tutte',
-    'background',
-    'paesaggi',
-    'sticker'
-  ];
+
+class ImageCategory {
+  List<String> list = ['tutte', 'background', 'paesaggi', 'sticker'];
 }
 
+class Furniture {
+  List<WidgetFurniture> list = [
+    ContainerFurniture(
+      id: 0,
+      type: WidgetType.container,
+      icon: Icons.crop_din,
+      selected: false,
+      width: 100,
+      height: 20,
+      color: kPrimaryColor,
+    ),
+    RowFurniture(
+      id: 0,
+      type: WidgetType.row,
+      icon: Icons.table_rows,
+      selected: false,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      mainAxisSize: MainAxisSize.min,
+    ),
+  ];
+}
