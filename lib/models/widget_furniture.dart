1a8952bcd1a53a2f9ac6fc47c3ab5f60d4bc14df
GiudaBallerino <lorispesarin@gmail.com>
2022-04-29 23:32:13 +0200

    modified forniture model

--- lib/models/widget_furniture.dart
+++ lib/models/widget_furniture.dart
@@ -1,134 +1,16 @@
 import 'package:flutter/material.dart';
 
 enum WidgetType {
-  container,
+  text,
+  image,
   row,
 }
-
-abstract class WidgetFurniture {
+class WidgetFurniture {
   WidgetFurniture({
     required this.type,
-    required this.selected,
     required this.icon,
-    //required this.id,
   });
 
   final WidgetType type;
   final IconData icon;
-  bool selected;
-  //int id;
-
-  Map<String, dynamic> toJson() => {};
-}
-
-// class RowFurniture extends WidgetFurniture {
-//   RowFurniture({
-//     required WidgetType type,
-//     required bool selected,
-//     required IconData icon,
-//     required int id,
-//     required this.crossAxisAlignment,
-//     required this.mainAxisAlignment,
-//     required this.mainAxisSize,
-//   }) : super(type: type, selected: selected, icon: icon, id:id);
-//
-//   CrossAxisAlignment crossAxisAlignment;
-//   MainAxisAlignment  mainAxisAlignment;
-//   MainAxisSize mainAxisSize;
-//
-//
-//   Map<String, dynamic> toJson() => {
-//     'type': type.name,
-//     'id':id,
-//     'args': {
-//       'crossAxisAlignment':crossAxisAlignment.toString().split('.').last,
-//       'mainAxisAlignment':mainAxisAlignment.toString().split('.').last,
-//       'mainAxisSize':mainAxisSize.toString().split('.').last,
-//     },
-//     'children': []
-//   };
-// }
-
-// class ContainerFurniture extends WidgetFurniture {
-//   ContainerFurniture({
-//     required WidgetType type,
-//     required bool selected,
-//     required IconData icon,
-//     required int id,
-//     //this.alignment,
-//     //this.clipBehavior,
-//     required this.color,
-//     //this.constraints,
-//     //this.decoration,
-//     required this.height,
-//     //this.margin,
-//     //this.padding,
-//     //this.transform,
-//     required this.width,
-//   }) : super(type: type, selected: selected, icon: icon,id: id);
-//
-//   // Alignment? alignment;
-//   // Clip? clipBehavior;
-//   Color color;
-//   //BoxConstraints? constraints;
-//   //BoxDecoration? decoration;
-//   double height;
-//   // EdgeInsets? margin;
-//   // EdgeInsets? padding;
-//   //Matrix4? transform;
-//   double width;
-//
-//   Map<String, dynamic> toJson() => {
-//         'type': type.name,
-//         'id':id,
-//         'selected':false,
-//         'args': {
-//           'color': '#${(color.value - 4278190080).toRadixString(16)}',
-//           'height': height,
-//           'width': width,
-//         },
-//       };
-// }
-
-class RowFurniture extends WidgetFurniture {
-  RowFurniture({
-    required WidgetType type,
-    required bool selected,
-    required IconData icon,
-    required this.widget,
-  }) : super(type: type, selected: selected, icon: icon);
-
-  Row widget;
-  // Map<String, dynamic> toJson() => {
-  //   'type': type.name,
-  //   'id':id,
-  //   'args': {
-  //     'crossAxisAlignment':crossAxisAlignment.toString().split('.').last,
-  //     'mainAxisAlignment':mainAxisAlignment.toString().split('.').last,
-  //     'mainAxisSize':mainAxisSize.toString().split('.').last,
-  //   },
-  //   'children': []
-  // };
-}
-
-class ContainerFurniture extends WidgetFurniture {
-  ContainerFurniture({
-    required WidgetType type,
-    required bool selected,
-    required IconData icon,
-    required this.widget,
-  }) : super(type: type, selected: selected, icon: icon);
-
-  Container widget;
-
-  // Map<String, dynamic> toJson() => {
-  //       'type': type.name,
-  //       'id':id,
-  //       'selected':false,
-  //       'args': {
-  //         'color': '#${(color.value - 4278190080).toRadixString(16)}',
-  //         'height': height,
-  //         'width': width,
-  //       },
-  //     };
 }
