0274e53cec9e9f273f30ed1e6a12ffcb90c9252b
GiudaBallerino <lorispesarin@gmail.com>
2022-05-16 22:31:58 +0200

    added container rendering

--- lib/screen/builder/components/render_furniture_widget.dart
+++ lib/screen/builder/components/render_furniture_widget.dart
@@ -9,7 +9,6 @@ class RenderWidget extends StatefulWidget {
       required this.furniture,
       this.maxWidth,
       required this.selected,
-      required this.fit,
       required this.onTap,
       this.onAccept})
       : super(key: key);
@@ -17,7 +16,6 @@ class RenderWidget extends StatefulWidget {
   Widget furniture;
   final double? maxWidth;
   final Widget? selected;
-  final FlexFit fit;
   Function(Widget) onTap;
   Function(WidgetFurniture)? onAccept;
 
@@ -31,13 +29,10 @@ class _RenderWidgetState extends State<RenderWidget> {
     Size size = MediaQuery.of(context).size;
     if (widget.furniture.runtimeType == Flexible) {
       Container container = (widget.furniture as Flexible).child as Container;
-      //Container container = widget.furniture as Container;
       return Flexible(
-        fit: widget.fit,
+        fit: FlexFit.loose,
         child: InkWell(
           child: Container(
-            //width: container.constraints?.maxWidth,
-            //height: container.constraints?.maxHeight,
             padding: EdgeInsets.all(5),
             decoration: BoxDecoration(
               color: container.color,
@@ -53,6 +48,27 @@ class _RenderWidgetState extends State<RenderWidget> {
           onTap: () => widget.onTap(widget.furniture),
         ),
       );
+    }else if (widget.furniture.runtimeType == Container) {
+        Container container = widget.furniture as Container;
+        return Flexible(
+          fit: FlexFit.loose,
+          child: InkWell(
+            child: Container(
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: container.color,
+                border: Border.all(
+                  color: widget.selected == widget.furniture
+                      ? kPrimaryColor
+                      : kDisabledColor,
+                  width: 3,
+                ),
+              ),
+              child: container.child,
+            ),
+            onTap: () => widget.onTap(widget.furniture),
+          ),
+        );
     } else if (widget.furniture.runtimeType == Row) {
       Row row = widget.furniture as Row;
       return DragTarget<WidgetFurniture>(
@@ -75,9 +91,9 @@ class _RenderWidgetState extends State<RenderWidget> {
                 ),
               ),
               child: Row(
-                //crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: row.crossAxisAlignment,
+                mainAxisAlignment: row.mainAxisAlignment,
+                mainAxisSize: row.mainAxisSize,
                 children: [
                   for (Widget child in row.children)
                     getRenderChild(child, row.children.length)!,
@@ -89,7 +105,7 @@ class _RenderWidgetState extends State<RenderWidget> {
           );
         },
         onWillAccept: (data) {
-          if (data?.type == WidgetType.row) {
+          if (data?.type == WidgetType.row || data.runtimeType != WidgetFurniture) {
             return false;
           } else {
             return true;
@@ -106,15 +122,19 @@ class _RenderWidgetState extends State<RenderWidget> {
     if (child.runtimeType == Flexible) {
       return RenderWidget(
         furniture: child as Flexible,
-        fit: FlexFit.tight,
         selected: widget.selected,
         onTap: (data) => widget.onTap(data),
       );
-    } else if (child.runtimeType == Row) {
+    } else if (child.runtimeType == Container) {
+      return RenderWidget(
+        furniture: child as Container,
+        selected: widget.selected,
+        onTap: (data) => widget.onTap(data),
+      );
+    }else if (child.runtimeType == Row) {
       return RenderWidget(
         maxWidth: (widget.maxWidth! - 16) / childrenLenght,
         furniture: child as Row,
-        fit: FlexFit.tight,
         selected: widget.selected,
         onTap: (data) => widget.onTap(data),
         onAccept: (data) => widget.onAccept!(data),
